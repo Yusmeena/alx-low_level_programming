f883d9dc2f026e872a4c711ad9c3af0adf368425
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-02-17 15:08:34 -0800

    Bi ithilahi, i use c program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,26 @@
-/*A program to generate 
-a positive or negative random number */
-
-#include <stdio.h> 
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - This program will assign a random number to the variable n
+ * Return: 0
+ */
 int main(void)
 {
-   int n;
+	int n;
 
-  srand(time(0);
-    n=rand()-RAND_MAX/2;
-
-if (n > 0)
-{
-    printf("This is a positive number");
-}
-    else if (n < 0)
-{
-     printf("This is a negative number");
-}
-
-else: 
-{
-    printf ("number is equal to zero");
-}
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+		printf("%d is negative\n", n);
+	return (0);
 }
