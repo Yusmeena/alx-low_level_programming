fe9392c1561af3f1a93e19f37be40552b211c237
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-02-16 17:27:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,7 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
- *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- * Return: Always 0 (Success)
+ * main - prints a string and standard error
+ * Return: 1
  */
-
-int main(void)
-{
-write(1,  "and that piece of art is useful\"  - Dora Korpar,  2015-10-19\n", 59);
+int main(void)                                       {                                                    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-}
