6e2e18c30337b5c029c4a4185683e3015005d15b
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-02-22 12:33:20 -0800

    add 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,26 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - main function
+ * add - adding it up
+ * @a: first integer
+ * @b: second integer
  *
- * Return: nothing
+ * Return: sum of both integers
  */
-int main(void)
+int add(int a, int b)
 {
-	int counter = 0;
-	long int a = 1;
-	long int b = a;
-	long int c = a + b;
-
-	while (c < 4000000)
-	{
-		if (c % 2 == 0)
-		{
-			counter += c;
-		}
-		a = b;
-		b = c;
-		c = a + b;
-	}
-	printf("%d\n", counter);
-	return (0);
+	return (a + b);
 }
