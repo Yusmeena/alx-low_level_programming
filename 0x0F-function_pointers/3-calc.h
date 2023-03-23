95883347bfd43c30503f45f4a62e2137d458fe07
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-03-22 17:32:34 -0700

    prototype2

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,20 +1,32 @@
-#ifndef _3_CALC_H
-#define _3_CALC_H
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdlib.h>
+
+/*
+ * Desc: Header file containing all structures and
+ *       prototypes used by the 3-main.c program.
+ */
+
 /**
- * struct op - Struct op
- *
+ * struct op - Entry point
+ * Description: Struct op
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
+/* Prototypes for functions used by main */
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 #endif
