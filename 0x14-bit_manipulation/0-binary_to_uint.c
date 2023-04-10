cdca4b6216a9c4b171bb3d7de9ae9f9c04b51fce
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-04-10 02:15:14 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (len--, base2_two = 1; len >= 0; len--, base2_two *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
@@ -29,7 +29,7 @@ unsigned int binary_to_uint(const char *b)
 
 		if (b[len] & 1)
 		{
-			bi += base_two;
+			bi += base2_two;
 		}
 	}
 
