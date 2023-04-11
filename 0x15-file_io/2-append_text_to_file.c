bf1c8d2962eeaf56a022da553092d4d552c6e1b5
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-04-10 19:48:19 -0700

    append text to the end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,8 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (numletters = 0; text_content[numletters]; numletters++)
-			;
+	for (numletters = 0; text_content[numletters]; numletters++);
 
 		rwr = write(filed, text_content, numletters);
 
