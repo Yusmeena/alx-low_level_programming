df72de30e6fa588db28958e1e5f776879940e82e
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-04 08:42:43 -0700

    last test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,32 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
+ * binary_to_uint- converts binary to number to an
  * unsigned int.
- * @b: binary.
+ * *@b: binary
  *
  * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int bi;
+	int leng, base2_two;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
+	bi = 0;
+	for (leng = 0; b[leng] != '\0'; leng++)
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (leng--; base2_two = 1; leng >= 0; leng--, base2_two, *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[leng] != '0' && b[leng] != '1')
 		{
 			return (0);
 		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
+		if (b[leng] & 1)
+		{ bi += base2_two;
 		}
 	}
-
-	return (ui);
+	return (bi);
 }
