87bf59f1b1408ea67dcfa120efe9e44826118dc3
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-04 14:29:54 -0700

    display index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,9 +2,9 @@
 #define _MAIN_H_
 
 unsigned int binary_to_uint(const char  *b);
-void print_binary(unsigned long int num);
+void print_binary(unsigned long int n);
 int _putchar(char c);
-int get_bit(unsigned long int num, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 #endif
