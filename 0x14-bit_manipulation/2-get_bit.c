2210c5f649ed486c62466a029540df62ca3199c4
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-04 15:01:55 -0700

    index to display

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,16 +8,17 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned int a;
-	if ( n == 0 && index < 64)
+	unsigned int a;
+	if (n == 0 && index < 64)
 		return (0);
-		
-		for  ( a = 0; a =< 63; n >>= 1;  a++)
-{
-	if (index == j)
-	{ 
-		return ( n & 1);
-}
-}
-	return(-1);
+	
+	for (a = 0; a  =<  63; n  >>=  1;  a++)
+	{
+		if (index == a)
+		{
+		return (n & 1);
+		}
+	}
+
+	return (-1);
 }
