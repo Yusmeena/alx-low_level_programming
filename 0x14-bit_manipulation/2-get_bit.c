9842811703db4dc734ba3d9369d98c568fb76599
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-04 17:00:00 -0700

    index of binary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,16 +4,15 @@
 *@n: unsigned long int input
 *@index: index of the bit
 *
-*Returns: return the value of the bit
+*Return: return the value of the bit
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int a;
 
 	if (n == 0 && index < 64)
-		return (0);
-	
-	for (a = 0; a  =<  63; n  >>=  1,  a++)
+	return (0);
+	for (a = 0; a <= 63; n >>= 1,  a++)
 	{
 		if (index == a)
 		{
