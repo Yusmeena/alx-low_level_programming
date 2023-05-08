427db6ffb2ea69147de37c352ef7c8d66200c46b
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-08 09:17:35 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if (text_context)
+	if (text_content)
 	{
 		for (letters = 0; text_content[letters]; letters++)
 			;
@@ -34,4 +34,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(file);
+
+	return (1);
 }
