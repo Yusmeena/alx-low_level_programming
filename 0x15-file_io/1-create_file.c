291b9248de7f5ca2b99a7893881c55bbe971dca3
Ojo Yusuf <ojoyusffemi1@gmail.com.com>
2023-05-08 04:41:59 -0700

    create a file and write to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: filename.
+* @text_content: content writed in the file.
+*
+* Return: 1 if it success, -1 if it fails.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int filed;
+	int numletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	filed = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+
+	if (filed == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (numletters = 0; text_content[numletters]; numletters++)
+	;
+
+	rwr = write(filed, text_content, numletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	closed(filed);
+
+	return (1);
+}
