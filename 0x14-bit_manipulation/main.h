eab37020bdf33da0dc34aa95bd9a3881b4fd2658
Ojo Yusuf <ojoyusffemi1@gmail.com>
2023-07-07 18:05:46 -0700

    bit manipulate

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,10 @@
-#ifndef MAIN_H
+#ifndef _MAIN_H
 
-#define MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
