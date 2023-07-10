17f6dcfa84190ed53de6a1a806bdbc816f2e1c99
Ojo Yusuf <ojoyusffemi1@gmail.com>
2023-07-10 12:57:20 -0700

    recheck

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,25 +8,26 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
+	int filed;
 	ssize_t, nrd, nwr;
 	char *buff;
 
 	if (!filename)
 		return (0);
-	file = open(filename, O_RDONLY);
 
-	if (file == -1)
+	filed = open(filename, O_RDONLY);
+
+	if (filed == -1)
 		return (0);
 
 	buff = malloc(sizeof(char) * (letters));
 	if (!buff)
 		return (0);
 
-	nrd = read(file, buff, letters);
+	nrd = read(filed, buff, letters);
 	nwr = write(STDOUT_FILENO, buff, nrd);
 
-	close(file);
+	close(filed);
 
 	free(buff);
 
